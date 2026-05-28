f3d073868238b2e8a148a9921c92eef7f02c40a3
diana <dianaaapricop@gmail.com>
2026-05-28 18:26:41 +0300

    BYE

--- collect potion.py
+++ collect potion.py
@@ -9,6 +9,9 @@ mushrooms=0
 crystals=0
 healing_potions=0
 Mana_potions=0
+wolf_pelts=0
+red_mushroom=0
+instant_damage=0
 
 while user!="quit":
     print("what u like to do?")
@@ -22,15 +25,18 @@ while user!="quit":
         print("Your currently have",herbs,"Herbs",mushrooms,"Mushrooms",crystals,"Crystals",healing_potions,"Healing potions",Mana_potions,"Mana potions.")
     
     elif choice=="2":
-        herbs+=1
-        mushrooms+=1
-        crystals+=1
-        print("You now have",herbs,"Herbs",mushrooms,"Mushrooms",crystals,"Crystals")
+        herbs+=3
+        mushrooms+=4
+        crystals+=2
+        red_mushroom+=3
+        wolf_pelts+=5
+        print("You now have",herbs,"Herbs",mushrooms,"Mushrooms",crystals,"Crystals", wolf_pelts,"wolf pelts",red_mushroom, "red mushrooms")
     
     elif choice=="3":
         print("What potion would you like to craft?")
         print("1.Healing potion, the recipe is 2 herbs and 1 mushrooms")
         print("2.Mana potion, the recipe is 1 herb,1 Mushroom,1 Crystal")
+        print("3.Instant damage potion, the reciep is 1 wolf pelt, 2 red mushrooms and 1 herb")
         potion=input("")
         
         if potion=="1":
@@ -50,6 +56,14 @@ while user!="quit":
                 crystals-=1
                 Mana_potions+=1
                 print("You have successfully crafted a Mana potion!!!","You currently have",Mana_potions,"Mana potions")
+
+        elif potion =="3":
+            if wolf_pelts >=1 and red_mushroom >=2 and herbs >=1:
+                herbs-=1
+                red_mushroom-=2
+                wolf_pelts-=1
+                instant_damage+=1
+                print("You have successfully crafted a instant damage potion!!!","You currently have",instant_damage,"instant damage potions")
     
     elif choice=="4":
         print("Exiting")
